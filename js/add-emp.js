3e025e6fe5973620304c19239b8a931e339c8547
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2024-11-12 18:16:03 +0530

    [Elavarasu] Add . Employee app

---
+++ js/add-emp.js
@@ -0,0 +1,239 @@
+$(window).on('load', () => {
+	const urlParams = new URLSearchParams(window.location.search)
+	let id = urlParams.get('id')
+
+	const $profileCtnRef = $('#profile-ctn')
+	for (let prof of [1, 2, 3, 4]) {
+		const $profileDiv = $('<label>', { class: 'flex gap-2' })
+
+		const $radio = $('<input>', {
+			type: 'radio',
+			id: `profile${prof}`,
+			name: `profile`,
+			value: `../assets/Profile/profile${prof}.png`,
+		})
+
+		const $img = $('<img>', {
+			src: `../assets/Profile/profile${prof}.png`,
+			id: `image${prof}`,
+			alt: `image${prof}`,
+		})
+
+		$profileDiv.append($radio, $img)
+		$profileCtnRef.append($profileDiv)
+	}
+
+	const GENDER = ['Male', 'Female']
+	const $genderCtnRef = $('#gender-ctn')
+	for (let gen of GENDER) {
+		const $genDiv = $('<label>', { class: 'flex gap-2' })
+
+		const $radio = $('<input>', {
+			type: 'radio',
+			id: gen,
+			name: 'gender',
+			value: gen,
+		})
+
+		const $label = $('<label>', {
+			for: gen,
+			text: gen,
+		})
+
+		$genDiv.append($radio, $label)
+		$genderCtnRef.append($genDiv)
+	}
+
+	const DEPARTMENTS = ['HR', 'Sales', 'Finance', 'Engineer', 'Others']
+	const $deptCtnRef = $('#dept-ctn')
+	for (let dept of DEPARTMENTS) {
+		const $deptDiv = $('<div>', { class: 'flex gap-2' })
+
+		const $checkbox = $('<input>', {
+			class: 'checkbox',
+			type: 'checkbox',
+			id: dept,
+			name: 'department',
+			value: dept,
+		})
+
+		const $label = $('<label>', {
+			for: dept,
+			text: dept,
+		})
+
+		$deptDiv.append($checkbox, $label)
+		$deptCtnRef.append($deptDiv)
+	}
+
+	const $yearCtnRef = $('#year')
+	const $monthCtnRef = $('#month')
+	const $dayCtnRef = $('#day')
+
+	let today = new Date().toUTCString().split(' ')
+	let year = parseInt(today[3])
+	const MONTHS = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec',
+	]
+	for (let i = year - 5; i <= year + 5; i++) {
+		const $yearOpt = $('<option>', {
+			value: i,
+			text: i,
+		})
+
+		$yearCtnRef.append($yearOpt)
+	}
+
+	for (let mon of MONTHS) {
+		const $monthOpt = $('<option>', {
+			value: mon,
+			text: mon,
+		})
+
+		$monthCtnRef.append($monthOpt)
+	}
+
+	const $monYearDiv = $('#month, #year')
+
+	$monYearDiv.on('change', () => {
+		let month = $('#month').val()
+		let year = $('#year').val()
+
+		if (month && year) {
+			$dayCtnRef.children().not(':first').remove()
+			let monthIndex = MONTHS.indexOf(month) + 1
+			let days = new Date(parseInt(year), monthIndex, 0).getDate()
+
+			let elements = Array.from({ length: days + 1 }, (_, i) => {
+				return $('<option>', {
+					value: i,
+					text: i,
+				})
+			})
+
+			$dayCtnRef.append(elements)
+		}
+	})
+
+	const PROPS = {
+		name: $('#name'),
+		profile: $('[name="profile"]'),
+		gender: $('[name="gender"]'),
+		department: $('[name="department"]'),
+		salary: $('#salary'),
+		salaryOutput: $('#salary-output'),
+		day: $('#day'),
+		month: $('#month'),
+		year: $('#year'),
+		notes: $('#notes'),
+	}
+
+	const getNextKey = () => {
+		let maxKey = 0
+
+		for (let i = 0; i < localStorage.length; i++) {
+			let key = localStorage.key(i)
+			// Check if the key is a number
+			if (!isNaN(key)) {
+				maxKey = Math.max(maxKey, parseInt(key, 10))
+			}
+		}
+		return maxKey + 1
+	}
+
+	// Adding a new item with the next key
+	const addItemToLocalStorage = data => {
+		if (id) {
+			data['id'] = id
+		} else {
+			data['id'] = getNextKey()
+		}
+		let key = data['id']
+		data = JSON.stringify(data)
+
+		localStorage.setItem(key, data)
+	}
+
+	const resetValues = () => {
+		// Reset text and number inputs
+		PROPS.name.val('')
+		PROPS.salary.val(50000)
+		PROPS.salaryOutput.text('50000')
+		PROPS.day.val('')
+		PROPS.month.val('')
+		PROPS.year.val('')
+		PROPS.notes.val('')
+		// Reset radio buttons (profile and gender)
+		PROPS.profile.prop('checked', false)
+		PROPS.gender.prop('checked', false)
+		// Reset checkbox (department)
+		PROPS.department.prop('checked', false)
+	}
+
+	const addEmployee = () => {
+		const data = {}
+		Object.entries(PROPS).forEach(([key, element]) => {
+			let type = element.prop('type')
+			if (type === 'radio') {
+				data[key] = element.filter(':checked').val() || null
+			} else if (type === 'checkbox') {
+				data[key] = element
+					.filter(':checked')
+					.map((_, el) => el.value)
+					.get()
+			} else {
+				data[key] = element.val()
+			}
+		})
+
+		addItemToLocalStorage(data)
+		// resetValues()
+	}
+
+	PROPS.salary.on('input', e => {
+		PROPS.salaryOutput.text(e.target.value)
+	})
+
+	if (id) {
+		let existingData = localStorage.getItem(id)
+		if (existingData) {
+			existingData = JSON.parse(existingData)
+		}
+
+		Object.entries(PROPS).forEach(([key, element]) => {
+			if (key != 'day') {
+				if (element.is(':radio')) {
+					element.filter(`[value="${existingData[key]}"]`).prop('checked', true)
+				} else {
+					element.val(existingData[key])
+				}
+			}
+		})
+
+		$monYearDiv.trigger('change')
+		PROPS.day.val(existingData.day)
+	}
+
+	let submitBtnRef = document.getElementById('submit')
+	submitBtnRef.addEventListener('click', e => {
+		e.preventDefault()
+		addEmployee()
+	})
+
+	let resetBtnRef = document.getElementById('reset')
+	resetBtnRef.addEventListener('click', e => {
+		e.preventDefault()
+		resetValues()
+	})
+})
---
+++ js/emp-list.js
@@ -0,0 +1,140 @@
+const empList = []
+let empListLength = localStorage.length
+
+for (let i = 0; i < empListLength; i++) {
+	let key = localStorage.key(i)
+	emp = JSON.parse(localStorage.getItem(key))
+	empList.push(emp)
+}
+
+const deleteEmp = id => {
+	$(`#row-${id}`).remove()
+	localStorage.removeItem(id)
+}
+
+const $tableBodyRef = $('#rows')
+
+for (let i = 0; i < empListLength; i++) {
+	const emp = empList[i]
+	const $rowRef = $('<tr>', {
+		class: 'bg-white',
+		id: `row-${emp.id}`,
+	})
+
+	let $tdProfile = $('<td>')
+
+	const $profileDivRef = $('<div>', {
+		class: 'flex justify-center items-center',
+	})
+
+	const $profileRef = $('<img>', {
+		src: emp.profile,
+		alt: 'emp',
+	})
+
+	$profileDivRef.append($profileRef)
+	$tdProfile.append($profileDivRef)
+	$rowRef.append($tdProfile)
+
+	let $tdNameRef = $('<td>')
+
+	const $nameDivRef = $('<div>', {
+		class: 'flex items-center justify-start',
+	})
+
+	const $nameSpanRef = $('<span>', {
+		text: emp.name,
+	})
+
+	$nameDivRef.append($nameSpanRef)
+	$tdNameRef.append($nameDivRef)
+	$rowRef.append($tdNameRef)
+
+	let $tdGenderRef = $('<td>')
+
+	const $genderDivRef = $('<div>', {
+		class: 'flex items-center justify-self-center',
+	})
+
+	const $genderSpanRef = $('<span>', {
+		text: emp.gender,
+	})
+
+	$genderDivRef.append($genderSpanRef)
+	$tdGenderRef.append($genderDivRef)
+	$rowRef.append($tdGenderRef)
+
+	let $tdDeptRef = $('<td>')
+
+	const $deptDivListRef = $('<div>', {
+		class: 'flex gap-4 justify-center',
+	})
+
+	for (let i = 0; i < emp.department.length; i++) {
+		const $deptDivRef = $('<div>', {
+			class: 'w-fit px-2 rounded-xl bg-lime-200',
+			text: emp.department[i],
+		})
+		$deptDivListRef.append($deptDivRef)
+	}
+	$tdDeptRef.append($deptDivListRef)
+	$rowRef.append($tdDeptRef)
+
+	let $tdSalaryRef = $('<td>')
+
+	const $salaryDivRef = $('<div>', {
+		class: 'flex items-center justify-self-center',
+	})
+
+	const $salarySpanRef = $('<span>', {
+		text: `$ ${emp.salary}`,
+	})
+	$salaryDivRef.append($salarySpanRef)
+	$tdSalaryRef.append($salaryDivRef)
+	$rowRef.append($tdSalaryRef)
+
+	let $tdDateRef = $('<td>')
+
+	const $dateDivRef = $('<div>', {
+		class: 'flex items-center justify-self-center',
+	})
+
+	const $dateSpanRef = $('<span>', {
+		text: `${emp.day} ${emp.month} ${emp.year}`,
+	})
+	$dateDivRef.append($dateSpanRef)
+	$tdDateRef.append($dateDivRef)
+	$rowRef.append($tdDateRef)
+
+	let $tdActionRef = $('<td>')
+
+	const $actionDivRef = $('<div>', {
+		class: 'flex gap-6 items-center justify-self-center',
+	})
+
+	const $imgUpdateRef = $('<img>', {
+		class: 'h-4 w-4 cursor-pointer',
+		src: '../assets/pen.png',
+		alt: 'edit',
+		id: emp.id,
+	})
+
+	$imgUpdateRef.click(() => {
+		window.location.href = `../html/add-emp-form.html?id=${emp.id}`
+	})
+
+	const $imgDeleteRef = $('<img>', {
+		class: 'h-4 w-4 cursor-pointer',
+		src: '../assets/delete.png',
+		alt: 'delete',
+		id: emp.id,
+	})
+
+	$imgDeleteRef.click(() => deleteEmp(emp.id))
+
+	$actionDivRef.append($imgUpdateRef, $imgDeleteRef)
+	$tdActionRef.append($actionDivRef)
+	$rowRef.append($tdActionRef)
+
+	$tableBodyRef.append($rowRef)
+}
